1043dd1f0950a0e225078c92992c280033dd0e1a
Long Tran <lngtrn94@gmail.com>
2019-09-26 15:12:17 +0200

    add sqlite install script

---
+++ databases/sqlite/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ZIP_FILE_NAME=sqlite-tools-linux-x86-3290000
+
+wget -O $ZIP_FILE_NAME.zip https://www.sqlite.org/2019/$ZIP_FILE_NAME.zip
+unzip $ZIP_FILE_NAME.zip
+mv $ZIP_FILE_NAME bin
+rm $ZIP_FILE_NAME.zip
