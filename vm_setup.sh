ee187cec3b4f6d0d0de57ccc86cfdf1227bf943e
Long Tran <lngtrn94@gmail.com>
2019-09-26 18:16:05 +0200

    add sqlite3 requirements to vm setup

--- vm_setup.sh
+++ vm_setup.sh
@@ -3,3 +3,8 @@
 apt update
 apt install -y cmake build-essential gcc gdb rsync zip python3-pip
 pip3 install pandas
+
+# SQLite3
+dpkg --add-architecture i386
+apt update
+apt install -y libc6:i386 libncurses5:i386 libstdc++6:i386 zlib1g:i386
