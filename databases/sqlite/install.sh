ab764efbedcb0c99af29ed8c20f770aa9e122023
Long Tran <lngtrn94@gmail.com>
2019-09-30 14:25:23 +0200

    compile sqlite from amalgamation source

--- databases/sqlite/install.sh
+++ databases/sqlite/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-ZIP_FILE_NAME=sqlite-tools-linux-x86-3290000
+ZIP_SOURCE=sqlite-amalgamation-3290000
 
-wget -O $ZIP_FILE_NAME.zip https://www.sqlite.org/2019/$ZIP_FILE_NAME.zip
-unzip $ZIP_FILE_NAME.zip
-mv $ZIP_FILE_NAME bin
-rm $ZIP_FILE_NAME.zip
+wget -O $ZIP_SOURCE.zip https://www.sqlite.org/2019/$ZIP_SOURCE.zip
+unzip $ZIP_SOURCE.zip
+mkdir bin
+gcc $ZIP_SOURCE/shell.c $ZIP_SOURCE/sqlite3.c -lpthread -ldl -o bin/sqlite3
+
+rm $ZIP_SOURCE.zip
+rm -rf $ZIP_SOURCE
