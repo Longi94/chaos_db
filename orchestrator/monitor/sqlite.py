1b0b3e66fe1fdc0e91755d493ab91f37c7e12b2e
Long Tran <lngtrn94@gmail.com>
2019-10-01 16:42:38 +0200

    add more result types

--- orchestrator/monitor/sqlite.py
+++ orchestrator/monitor/sqlite.py
@@ -5,12 +5,16 @@ from result import *
 
 class SQLiteMonitor(ProcessMonitor):
     def evaluate_result(self):
-        if self.exited and self.return_code == 0:
-            if self.compare_files(os.path.join(self.directory, 'output.txt').format(self.query),
-                                  'databases/sqlite/answers/q{}.out'.format(self.query)):
-                self.result = RESULT_OK
+        if self.exited:
+            if self.return_code == 0:
+                if self.compare_files(os.path.join(self.directory, 'output.txt').format(self.query),
+                                      'databases/sqlite/answers/q{}.out'.format(self.query)):
+                    self.result = RESULT_OK
+                else:
+                    self.result = RESULT_INCORRECT_OUTPUT
             else:
-                self.result = RESULT_INCORRECT_OUTPUT
+                self.result = RESULT_ABNORMAL_BEHAVIOR
+        elif self.signaled:
+            self.result = RESULT_CRASH
         else:
-            # TODO
-            pass
+            self.result = RESULT_UNKNOWN
