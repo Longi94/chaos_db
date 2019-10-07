f444025f1cf008b8aa38fb109c1aa26198eddbe0
Long Tran <lngtrn94@gmail.com>
2019-10-07 11:16:03 +0200

    jupyter config

---
+++ jupyter/jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+## The IP address the notebook server will listen on.
+c.NotebookApp.ip = '*'
+
+## Whether to open in a browser after starting. The specific browser used is
+#  platform dependent and determined by the python standard library `webbrowser`
+#  module, unless it is overridden using the --browser (NotebookApp.browser)
+#  configuration option.
+c.NotebookApp.open_browser = False
--- vm_setup.sh
+++ vm_setup.sh
@@ -4,6 +4,9 @@ apt update
 apt install -y cmake build-essential gcc gdb rsync zip python3-pip
 pip3 install pandas jupyter
 
+mkdir /home/vagrant/.jupyter
+cp /vagrant/jupyter/jupyter_notebook_config.py /home/vagrant/.jupyter/
+
 # SQLite3
 dpkg --add-architecture i386
 apt update
