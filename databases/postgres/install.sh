4257efb2a35e44a366243895444b5c3072074612
Long Tran <lngtrn94@gmail.com>
2019-10-14 15:29:51 +0200

    postgres install script

---
+++ databases/postgres/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(realpath $1)
+VERSION=11.5
+POSTGRES_DIR=postgresql-${VERSION}
+
+if [[ ! -f ${DIR}/${POSTGRES_DIR}.tar.bz2 ]]; then
+    wget -O ${DIR}/${POSTGRES_DIR}.tar.bz2 https://ftp.postgresql.org/pub/source/v${VERSION}/${POSTGRES_DIR}.tar.bz2
+fi
+
+tar xvf ${DIR}/${POSTGRES_DIR}.tar.bz2 -C ${DIR}/
+
+mkdir ${DIR}/bin
+cd ${POSTGRES_DIR}
+./configure --prefix ${DIR}/bin/ --disable-strict --disable-cassert CFLAGS="-O3"
+make
+make install
+
+rm -rf ${DIR}/${POSTGRES_DIR}
