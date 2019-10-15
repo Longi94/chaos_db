d057f9746718ade11f720b42d580983e89a487b4
Long Tran <lngtrn94@gmail.com>
2019-10-15 15:32:14 +0200

    fix postgres scripts

--- databases/postgres/load.sql
+++ databases/postgres/load.sql
@@ -1,8 +1,8 @@
-COPY customer FROM '../tpch/tables/customer.tbl' WITH DELIMITER AS '|';
-COPY lineitem FROM '../tpch/tables/lineitem.tbl' WITH DELIMITER AS '|';
-COPY nation FROM '../tpch/tables/nation.tbl' WITH DELIMITER AS '|';
-COPY orders FROM '../tpch/tables/orders.tbl' WITH DELIMITER AS '|';
-COPY part FROM '../tpch/tables/part.tbl' WITH DELIMITER AS '|';
-COPY partsupp FROM '../tpch/tables/partsupp.tbl' WITH DELIMITER AS '|';
-COPY region FROM '../tpch/tables/region.tbl' WITH DELIMITER AS '|';
-COPY supplier FROM '../tpch/tables/supplier.tbl' WITH DELIMITER AS '|';
+\copy customer FROM '../tpch/tables/customer.tbl' WITH DELIMITER AS '|';
+\copy lineitem FROM '../tpch/tables/lineitem.tbl' WITH DELIMITER AS '|';
+\copy nation FROM '../tpch/tables/nation.tbl' WITH DELIMITER AS '|';
+\copy orders FROM '../tpch/tables/orders.tbl' WITH DELIMITER AS '|';
+\copy part FROM '../tpch/tables/part.tbl' WITH DELIMITER AS '|';
+\copy partsupp FROM '../tpch/tables/partsupp.tbl' WITH DELIMITER AS '|';
+\copy region FROM '../tpch/tables/region.tbl' WITH DELIMITER AS '|';
+\copy supplier FROM '../tpch/tables/supplier.tbl' WITH DELIMITER AS '|';
