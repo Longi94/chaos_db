bcbf2599884e0e3852e644098e6510bbc9cd2755
Long Tran <lngtrn94@gmail.com>
2019-10-31 12:37:27 +0100

    generate updates

--- databases/tpch/dbgen.sh
+++ databases/tpch/dbgen.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-git clone https://github.com/gregrahn/tpch-kit
+git clone https://github.com/gregrahn/tpch-kit || true
 cd tpch-kit/dbgen
 make
 ./dbgen -v -f -s 1
+./dbgen -v -U 1 -s 1
 
 cd ../..
-mkdir tables
+mkdir tables 2>/dev/null || true
 mv tpch-kit/dbgen/*.tbl tables/
+
+mkdir updates 2>/dev/null || true
+mv tpch-kit/dbgen/*.tbl.u1 updates/
