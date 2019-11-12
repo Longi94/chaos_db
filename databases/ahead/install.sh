60b783d32692b01b34eb81c58f9d7317ad82b5db
Long Tran <lngtrn94@gmail.com>
2019-11-12 15:32:12 +0100

    use master branch

--- databases/ahead/install.sh
+++ databases/ahead/install.sh
@@ -9,14 +9,12 @@ die () {
 
 [[ $# -ge 1 ]] || die "1 argument required, $# provided"
 
-VERSION=20e0f9c6b99daff0ab36e942310ffefc3d93751e
 DIR=$(realpath $1)
 
 pushd ${DIR}
 git clone https://github.com/Longi94/AHEAD.git
 pushd AHEAD
 git submodule update --init --recursive
-git checkout ${VERSION} -b ${VERSION}
 
 ./bootstrap.sh
 
