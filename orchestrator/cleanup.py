756bc1f3a8ccc773cab45dbd47df052844d42546
Long Tran <lngtrn94@gmail.com>
2019-11-19 15:24:48 +0100

    cleanup script

---
+++ orchestrator/cleanup.py
@@ -0,0 +1,20 @@
+import os
+
+for exp_name in os.listdir('.'):
+
+    db = exp_name.split('_')[0]
+
+    if db not in ('sqlite',):
+        continue
+
+    for iteration in os.listdir(exp_name):
+
+        if iteration in ('experiment.log', 'results.sqlite'):
+            continue
+
+        if db == 'sqlite':
+            output_file = os.path.join(exp_name, iteration, 'output.txt')
+
+            if os.path.getsize(output_file) > 1000000:
+                print(f'Removing {output_file}...')
+                os.remove(output_file)
