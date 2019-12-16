b770f4d6750b7a29d96740309572be0160da9676
Long Tran <lngtrn94@gmail.com>
2019-12-16 16:43:58 +0100

    remove update prints

--- databases/sqlite/update.sql
+++ databases/sqlite/update.sql
@@ -1,6 +1,4 @@
 .mode csv
 .sep |
-.print "Importing orders.tbl.u1..."
 .import {DIR}/../tpch/updates/orders.tbl.u1 orders
-.print "Importing lineitem.tbl.u1..."
 .import {DIR}/../tpch/updates/lineitem.tbl.u1 lineitem
